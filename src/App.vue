b4ee59f5de72a29934e6b7c98cb54e2ee62efdd8
atilla kalay <atilla.kalayy@gmail.com>
2024-07-08 22:52:54 +0300

    Lesson 7 has been complated.

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+!<template>
+  <Navbar />
+  <RouterView />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,200..1000;1,200..1000&display=swap");
+
+html,
+body {
+  font-family: "Mulish", sans-serif;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import BooksView from "@/views/BooksView.vue";
+import ContactView from "@/views/ContactView.vue";
+import HomeView from "@/views/HomeView.vue";
+import LoginView from "@/views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+    { path: "/", component: HomeView, name: "home" },
+    { path: "/books", component: BooksView, name: "books" },
+    { path: "/contact", component: ContactView, name: "contact" },
+    { path: "/login", component: LoginView, name: "login" },
+    { path: "/register", component: RegisterView, name: "register" }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+    linkActiveClass: 'active-link'
+});
+
+export default router;
