ca0da159405b1e3ddbb57ccbad5b23a10a85626c
atillakalay <atilla.kalayy@gmail.com>
2024-07-11 09:15:13 +0300

    Lesson 12 has been complated.

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,16 @@ import ContactView from "@/views/ContactView.vue";
 import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
+import BookDetailView from "@/views/BookDetailView.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     { path: "/", component: HomeView, name: "home" },
     { path: "/books", component: BooksView, name: "books" },
+    { path: "/books/:id", component: BookDetailView, name: "book-detail" },
     { path: "/contact", component: ContactView, name: "contact" },
     { path: "/login", component: LoginView, name: "login" },
-    { path: "/register", component: RegisterView, name: "register" }
+    { path: "/register", component: RegisterView, name: "register" },
 ];
 
 const router = createRouter({
